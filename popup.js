d33eedb2ccf1d1add30eea863edc1dd81e396c51
Joel Mariscal <50393673+jomarisc@users.noreply.github.com>
2025-02-04 12:32:56 -0800

    all functionality implemented

--- popup.js
+++ popup.js
@@ -1,9 +1,23 @@
-console.log('Hello, Yuliana!')
-
-  const getUrl = async () => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    console.log(tab.url);
-    console.log('Yeah, this worked.')
+console.log('Hello, Yuliana!');
+  const yesbutton = document.getElementById('yes');
+  yesbutton.onclick = () => {
+    chrome.tabs.update(undefined, {url: 'https://digitalnest.org'});
   }
 
-  getUrl();
+  const laterbutton = document.getElementById('later');
+  laterbutton.onclick = () => {
+    window.close();
+  }
+
+  chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+      if(changeInfo.url){
+        alert('Wowwwww')
+      }
+      console.log(tab.url.includes('amazon'))
+      if(tab.url.includes('amazon')) {
+        console.log('wow')
+        chrome.action.openPopup()
+      }
+    }
+  );
